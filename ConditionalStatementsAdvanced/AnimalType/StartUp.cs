0736b1cac74ea8224f9fac9bdc4223db3e333dee
Victor Stanchev <doubledoublelift@abv.bg>
2021-05-13 00:28:12 +0300

    ok

---
+++ ConditionalStatementsAdvanced/AnimalType/StartUp.cs
@@ -0,0 +1,34 @@
+﻿
+namespace AnimalType
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            string output = "";
+
+            switch (animal)
+            {
+                case "dog":
+                    output = "mammal";
+                    break;
+
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    output = "reptile";
+                    break;
+
+                default:
+                    output = "unknown";
+                    break;
+
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
